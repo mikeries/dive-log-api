7c7bf2d10934a4cf926704d9cfdb717f26c72308
Michael Ries <michael.ries@gmail.com>
2017-07-13 10:48:57 -0500

    add user model

---
+++ db/migrate/20170713154137_create_users.rb
@@ -0,0 +1,12 @@
+class CreateUsers < ActiveRecord::Migration[5.1]
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :email
+      t.string :image
+      t.integer :uid, limit: 8
+
+      t.timestamps
+    end
+  end
+end

42a581bd8608f1892c5eb903a4a70f650574fc73
Michael Ries <michael.ries@gmail.com>
2017-07-13 12:52:16 -0500

    add github authentication

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   root 'welcome#home'
 
   get '/auth/facebook/callback' => 'sessions#create'
+  get '/auth/github/callback' => 'sessions#create'
+  
   get '/authenticate' => 'sessions#authenticate'
 end

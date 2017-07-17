060b17ed8bddb1215dce10ffd88eef830a58f984
Michael Ries <michael.ries@gmail.com>
2017-07-17 13:01:59 -0500

    fill in more data in the DiveSerializer

--- app/serializers/dive_serializer.rb
+++ app/serializers/dive_serializer.rb
@@ -1,4 +1,6 @@
 class DiveSerializer < ActiveModel::Serializer
-  attributes :id, :datetime
+  attributes :id, :datetime, :location_id, :datetime,
+             :duration, :ballast, :max_depth, 
+             :starting_pressure, :final_pressure, :comments
   belongs_to :location
 end

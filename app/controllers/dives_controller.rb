79ebb6f1ef232694d86802e1b9a95823808a5e83
Michael Ries <michael.ries@gmail.com>
2017-07-18 13:18:32 -0500

    add #update method

--- app/controllers/dives_controller.rb
+++ app/controllers/dives_controller.rb
@@ -6,4 +6,26 @@ class DivesController < ApplicationController
     render json: User.first.dives
   end
 
+  def update
+    dive = User.first.dives.find(params[:id])
+    dive.update(dive_params)
+    render json: {}
+  end
+
+  private
+
+  def dive_params
+    params.require(:dive).permit(
+      :id, 
+      :datetime, 
+      :location_id, 
+      :duration, 
+      :ballast,
+      :max_depth,
+      :starting_pressure,
+      :final_pressure,
+      :comments
+    )
+  end
+
 end

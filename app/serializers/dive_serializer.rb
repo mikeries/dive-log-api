9a6b1a41aa3c69a2c3e736c84e04c08f963106c7
Michael Ries <michael.ries@gmail.com>
2017-07-19 10:31:49 -0500

    split datetime into date and time

--- app/serializers/dive_serializer.rb
+++ app/serializers/dive_serializer.rb
@@ -1,5 +1,5 @@
 class DiveSerializer < ActiveModel::Serializer
-  attributes :id, :datetime, :location_id, :date, :time,
+  attributes :id, :location_id, :date, :time,
              :duration, :ballast, :max_depth,
              :starting_pressure, :final_pressure, :comments
   belongs_to :location

6b3005288bc7bb42b2ba756cb2595e8bd97fd651
Michael Ries <michael.ries@gmail.com>
2017-07-26 13:49:46 -0500

    refactorin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,22 +9,22 @@ class ApplicationController < ActionController::API
     return @current_user if @current_user
     if auth_present?
       uid = Auth.decode_uid(read_token_from_request)
-      @current_user = User.find_by({uid: uid})
+      @current_user = User.find_by(uid: uid)
       return @current_user if @current_user
     end
   end
 
   def authenticate
-    render json: {error: "unauthorized"}, status: 401 unless logged_in?
+    render json: { error: 'unauthorized' }, status: 401 unless logged_in?
   end
 
   private
 
   def read_token_from_request
-    token = request.env["HTTP_AUTHORIZATION"].scan(/Bearer: (.*)$/).flatten.last
+    request.env['HTTP_AUTHORIZATION'].scan(/Bearer: (.*)$/).flatten.last
   end
 
   def auth_present?
-    !!request.env.fetch("HTTP_AUTHORIZATION", "").scan(/Bearer/).flatten.first
+    !!request.env.fetch('HTTP_AUTHORIZATION', '').scan(/Bearer/).flatten.first
   end
 end

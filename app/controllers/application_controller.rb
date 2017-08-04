0620c28561ae45e5be2a2963a25ec0a6230ebf1e
Michael Ries <michael.ries@gmail.com>
2017-08-04 13:35:21 -0500

    still more debug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,6 +17,7 @@ class ApplicationController < ActionController::API
   def facebook_token_valid?(token)
     response = Faraday.get("https://graph.facebook.com/app?access_token=#{token}")
     data = JSON.parse(response.body)
+    puts 'App ID:' + data['id']
     return true if data['id'] == ENV['FACEBOOK_CLIENT_APP_ID']
     false
   end
